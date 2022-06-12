20adeaf5499cf25c9de882f11e8d9be4038c049c
SteveLtt <102153470+SteveLtt@users.noreply.github.com>
2022-06-12 13:04:19 +0800

    update

--- app/src/main/java/com/example/mobileapp_/ProfileActivity.java
+++ app/src/main/java/com/example/mobileapp_/ProfileActivity.java
@@ -1,14 +1,69 @@
 package com.example.mobileapp_;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+
+    private EditText email_value,name_value,age_value;
+    private DatabaseReference reference;
+
+    private String UserId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        UserId = user.getUid();
+
+
+        final TextView emailTextView = (TextView) findViewById(R.id.email_value);
+        final TextView nameTextView = (TextView) findViewById(R.id.name_value);
+        final TextView ageTextView = (TextView) findViewById(R.id.age_value);
+
+
+        reference.child(UserId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if(userProfile != null){
+                    String name = userProfile.name;
+                    String email =userProfile.email;
+                    String age = userProfile.age;
+
+                    nameTextView.setText(name);
+                    emailTextView.setText(email);
+                    ageTextView.setText(age);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this,"Something wrong happened!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
