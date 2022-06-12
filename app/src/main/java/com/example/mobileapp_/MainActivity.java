5f2f829df3392ff388ffe0c43a831b8acb5a071d
SteveLtt <102153470+SteveLtt@users.noreply.github.com>
2022-06-12 23:51:31 +0800

    update list

--- app/src/main/java/com/example/mobileapp_/MainActivity.java
+++ app/src/main/java/com/example/mobileapp_/MainActivity.java
@@ -54,7 +54,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         List<RecentsData> recentsDataList = new ArrayList<>();
-
+        recentsDataList.add(new RecentsData("Automatic umbrellas","Type:Automatic","$200",R.drawable.automatic_umbrellas));
+        recentsDataList.add(new RecentsData("Collapsible umbrella","Type:Manual","$300",R.drawable.collapsible_umbrella));
+        recentsDataList.add(new RecentsData("Umbrella","Type:Manual","$400",R.drawable.umbrella));
+        recentsDataList.add(new RecentsData("Oil paper umbrella","Type:Automatic","$150",R.drawable.oil_paper_umbrella));
+        recentsDataList.add(new RecentsData("Parasol","Type:Manual","$400",R.drawable.parasol));
+        recentsDataList.add(new RecentsData("Hand-crafted umbrellas","Type:Manual","$500",R.drawable.hand_crafted_umbrellas));
 
         setRecentRecycler(recentsDataList);
 
