f1cf6191f91c151194c9a5e513bce3761334936c
SteveLtt <102153470+SteveLtt@users.noreply.github.com>
2022-06-12 18:15:34 +0800

    gps

--- app/src/main/java/com/example/mobileapp_/MapsActivity.java
+++ app/src/main/java/com/example/mobileapp_/MapsActivity.java
@@ -1,24 +1,58 @@
 package com.example.mobileapp_;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.mobileapp_.databinding.ActivityMaps2Binding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.mobileapp_.databinding.ActivityMaps2Binding;
+//import com.example.mobileapp_.databinding.ActivityMaps3Binding;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMaps2Binding binding;
-
-
+    private static final int DEFAULT_ZOOM = 15;
+    private Location lastLacation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private  static  final int REQUEST_CODE=101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +61,173 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
 
 
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getCurrentLocation();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        direction();
+//        LatLng sydney = new LatLng(lastLacation.getLatitude(), lastLacation.getLongitude());
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Your location"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLacation.getLatitude(), lastLacation.getLongitude()),DEFAULT_ZOOM));
+//        mMap.getUiSettings().isZoomControlsEnabled();
+
+
+    }
+
+
+    private void direction(){
+        String location = lastLacation.getLatitude()+", "+lastLacation.getLongitude();
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = Uri.parse("https://maps.googleapis.com/maps/api/directions/json")
+                .buildUpon()
+                .appendQueryParameter("destination","22.312668960717374, 114.22520054626735")
+                .appendQueryParameter("origin",location)
+                .appendQueryParameter("mode","walking")
+                .appendQueryParameter("key","AIzaSyB4mf1Nn4i4ciYyvYZqpuT-CSeHKCdAfDk")
+                .toString();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String status = response.getString("status");
+                    if (status.equals("OK")) {
+                        JSONArray routes = response.getJSONArray("routes");
+
+                        ArrayList<LatLng> points;
+                        PolylineOptions polylineOptions = null;
+
+                        for (int i = 0; i < routes.length(); i++) {
+                            points = new ArrayList<>();
+                            polylineOptions = new PolylineOptions();
+                            JSONArray legs = routes.getJSONObject(i).getJSONArray("legs");
+
+                            for (int a = 0; a < legs.length(); a++) {
+                                JSONArray steps = legs.getJSONObject(a).getJSONArray("steps");
+                                for (int q = 0; q < steps.length(); q++) {
+                                    String polyline = steps.getJSONObject(q).getJSONObject("polyline").getString("points");
+                                    List<LatLng> list = decodePoly(polyline);
+
+                                    for (int z = 0; z < list.size(); z++) {
+                                        LatLng position = new LatLng((list.get(z)).latitude, (list.get(z)).longitude);
+                                        points.add(position);
+                                    }
+                                }
+                            }
+                            polylineOptions.addAll(points);
+                            polylineOptions.width(10);
+                            polylineOptions.color(ContextCompat.getColor(MapsActivity.this, R.color.purple_500));
+                            polylineOptions.geodesic(true);
+                        }
+                        mMap.addPolyline(polylineOptions);
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(lastLacation.getLatitude(), lastLacation.getLongitude())).title("Your location"));
+                        mMap.addMarker(new MarkerOptions().position(new LatLng(22.312668960717374, 114.22520054626735)).title("Shop"));
+
+                        LatLngBounds bounds = new LatLngBounds.Builder()
+                                .include(new LatLng(lastLacation.getLatitude(), lastLacation.getLongitude()))
+                                .include(new LatLng(22.312668960717374, 114.22520054626735)).build();
+                        Point point = new Point();
+                        getWindowManager().getDefaultDisplay().getSize(point);
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, point.x, 150, 30));
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(retryPolicy);
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    private  List<LatLng> decodePoly(String encoded){
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng =0;
+
+        while (index<len){
+            int b, shift =0,result=0;
+            do{
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift +=5;
+            }while (b>=0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift =0;
+            result =0;
+
+            do {
+                b =encoded.charAt(index++)-63;
+                result |= (b & 0x1f) << shift;
+                shift +=5;
+            }while (b>=0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+            (((double) lng / 1E5)));
+
+            poly.add(p);
+        }
+
+
+        return poly;
+    }
+
+
+    private void getCurrentLocation(){
+        if(ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+            },REQUEST_CODE);
+            return;
+        }
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    lastLacation = location;
+                    //Toast.makeText(getApplicationContext(),(int) lastLacation.getLatitude(),Toast.LENGTH_LONG).show();
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MapsActivity.this);
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (REQUEST_CODE){
+            case REQUEST_CODE:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getCurrentLocation();
+                }
+        }
     }
 }
