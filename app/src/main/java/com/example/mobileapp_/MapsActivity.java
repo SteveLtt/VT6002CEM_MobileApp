a9812f9419855f0b0229028de35f75a7a6c8fbc4
SteveLtt <102153470+SteveLtt@users.noreply.github.com>
2022-06-12 23:29:44 +0800

    bugs fixed

--- app/src/main/java/com/example/mobileapp_/MapsActivity.java
+++ app/src/main/java/com/example/mobileapp_/MapsActivity.java
@@ -12,6 +12,7 @@ import android.graphics.Point;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -23,6 +24,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.mobileapp_.databinding.ActivityMaps2Binding;
+import com.example.mobileapp_.databinding.ActivityMapsBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -52,7 +54,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int DEFAULT_ZOOM = 15;
     private Location lastLacation;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private  static  final int REQUEST_CODE=101;
+    private static final int REQUEST_CODE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,22 +81,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         direction();
 //        LatLng sydney = new LatLng(lastLacation.getLatitude(), lastLacation.getLongitude());
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Your location"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLacation.getLatitude(), lastLacation.getLongitude()),DEFAULT_ZOOM));
+//        Log.d("xylication",lastLacation.getLatitude()+", "+lastLacation.getLongitude());
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(22.310470306212988, 114.22429238096183),DEFAULT_ZOOM));
 //        mMap.getUiSettings().isZoomControlsEnabled();
-
-
+//
+//        LatLng sydney = new LatLng(22.310470306212988, 114.22429238096183);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, DEFAULT_ZOOM));
     }
 
 
-    private void direction(){
-        String location = lastLacation.getLatitude()+", "+lastLacation.getLongitude();
+    private void direction() {
+        String location = lastLacation.getLatitude() + ", " + lastLacation.getLongitude();
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         String url = Uri.parse("https://maps.googleapis.com/maps/api/directions/json")
                 .buildUpon()
-                .appendQueryParameter("destination","22.312668960717374, 114.22520054626735")
-                .appendQueryParameter("origin",location)
-                .appendQueryParameter("mode","walking")
-                .appendQueryParameter("key","AIzaSyB4mf1Nn4i4ciYyvYZqpuT-CSeHKCdAfDk")
+                .appendQueryParameter("destination", "22.312668960717374, 114.22520054626735")
+                .appendQueryParameter("origin", location)
+                .appendQueryParameter("mode", "walking")
+                .appendQueryParameter("key", "AIzaSyB4mf1Nn4i4ciYyvYZqpuT-CSeHKCdAfDk")
                 .toString();
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -158,34 +164,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private  List<LatLng> decodePoly(String encoded){
+    private List<LatLng> decodePoly(String encoded) {
         List<LatLng> poly = new ArrayList<>();
         int index = 0, len = encoded.length();
-        int lat = 0, lng =0;
+        int lat = 0, lng = 0;
 
-        while (index<len){
-            int b, shift =0,result=0;
-            do{
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
                 b = encoded.charAt(index++) - 63;
                 result |= (b & 0x1f) << shift;
-                shift +=5;
-            }while (b>=0x20);
+                shift += 5;
+            } while (b >= 0x20);
             int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
             lat += dlat;
 
-            shift =0;
-            result =0;
+            shift = 0;
+            result = 0;
 
             do {
-                b =encoded.charAt(index++)-63;
+                b = encoded.charAt(index++) - 63;
                 result |= (b & 0x1f) << shift;
-                shift +=5;
-            }while (b>=0x20);
+                shift += 5;
+            } while (b >= 0x20);
             int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
             lng += dlng;
 
             LatLng p = new LatLng((((double) lat / 1E5)),
-            (((double) lng / 1E5)));
+                    (((double) lng / 1E5)));
 
             poly.add(p);
         }
@@ -195,13 +201,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    private void getCurrentLocation(){
-        if(ActivityCompat.checkSelfPermission(
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION
-            },REQUEST_CODE);
+            }, REQUEST_CODE);
             return;
         }
 
@@ -209,13 +215,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                if(location != null){
+                if (location != null) {
                     lastLacation = location;
                     //Toast.makeText(getApplicationContext(),(int) lastLacation.getLatitude(),Toast.LENGTH_LONG).show();
                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
                     mapFragment.getMapAsync(MapsActivity.this);
 
                 }
+//                else{
+//                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+//                    mapFragment.getMapAsync(MapsActivity.this);
+//                }
             }
         });
     }
@@ -223,9 +233,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (REQUEST_CODE){
+        switch (REQUEST_CODE) {
             case REQUEST_CODE:
-                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getCurrentLocation();
                 }
         }
